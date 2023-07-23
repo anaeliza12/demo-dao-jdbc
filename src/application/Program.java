54989902f72eb37c2b67684756cefc5cb2eb98c2
Ana Eliza <anaelizaperobelli@gmail.com>
2023-07-23 20:49:06 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,12 +1,18 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String[] args) {
-	
+
 		Department d = new Department(1, "Books");
+
+		Seller s = new Seller(1, "Ana ", "anaeliza@gmail.com", new Date(), 3000.0);
+		
+		System.out.println(s);
 	}
 
 }
- 
