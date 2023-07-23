2c5cf6d191cce33c6b9aec89067a1fbbdf82b0c4
Ana Eliza <anaelizaperobelli@gmail.com>
2023-07-23 20:33:12 -0300

    Department entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,12 @@
 package application;
 
-import java.sql.Connection;
-
-import db.DB;
+import model.entities.Department;
 
 public class Program {
 	public static void main(String[] args) {
-		
-
+	
+		Department d = new Department(1, "Books");
 	}
 
 }
+ 
